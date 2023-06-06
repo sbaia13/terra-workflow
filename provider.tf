f19010cc7eee759b09e2e365f958cf8720efd845
sbaia13 <117842419+sbaia13@users.noreply.github.com>
2023-06-06 14:50:48 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,19 @@
 # Azure Provider source and version being used
 terraform {
+  required_version = ">= 1.3"
+  backend "azurerm" {
+    resource_group_name  = "backend"
+    storage_account_name = "backendgithubworkflow"
+    container_name       = "tfstate"
+    key                  = "actions.tfstate"
+  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.51.0"
+      version = "=3.2.0"
     }
   }
 }
-
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
